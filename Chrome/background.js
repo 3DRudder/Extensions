c73711c788d8837728c0a2992278f51e6c82f39a
Unknown <mvivant@3drudder.com>
2018-02-14 17:52:30 +0100

    Initial commit

---
+++ Chrome/background.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.browserAction.setBadgeText({text: "10+"}); // We have 10+ unread items.
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
+});
---
+++ Chrome/content-script.js
@@ -0,0 +1,27 @@
+/*
+Injects frame-by-frame.js into YouTube page.
+Content scripts are executed in an isolated environment, so to manipulate the player
+need to execute script into the page.
+http://stackoverflow.com/questions/9515704/building-a-chrome-extension-inject-code-in-a-page-using-a-content-script
+https://developer.chrome.com/extensions/content_scripts.html#execution-environment
+*/
+
+var s = document.createElement('script');
+s.src = chrome.extension.getURL("3dRudder-1.0.3.js");
+s.onload = function() {
+    console.log("3drudder loaded");
+    var j = document.createElement('script');
+    j.src = chrome.extension.getURL("video.js");
+    (document.head||document.documentElement).appendChild(j);
+    console.log("video started");
+};
+(document.head||document.documentElement).appendChild(s);
+console.log("3drudder Started!");
+
+/*chrome.storage.sync.set({'value': window.location.href }, function() {
+    // Notify that we saved.
+    console.log('Settings saved');
+  });*/
+  chrome.storage.sync.get(['value'], function(items) {
+    console.log('Settings retrieved', items);
+    });
