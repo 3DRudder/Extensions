1c382f37199779c47eeb64d6d6fcf3dab5f9bd93
Unknown <mvivant@3drudder.com>
2018-03-05 18:05:49 +0100

    add info axis

--- Chrome/options.js
+++ Chrome/options.js
@@ -30,8 +30,32 @@ function resetDefault() {
 document.addEventListener('DOMContentLoaded', () => {
             
     restoreDefault();    
-    $("#reset").click(resetDefault);
+    $("#reset").click(resetDefault);    
+    
+    var SDK = new Sdk3dRudder();
+    SDK.init();
+    SDK.on('frame', function(frame){
+        var controller = frame.controllers[0];
+        if (controller && controller.connected) {
+            var current_progress = parseInt(controller.axis[settings.playpause.axis] * 100);
+            var pos = current_progress > 0 ? current_progress : 0;
+            var neg = current_progress < 0 ? -current_progress : 0;
+            $("#playpausePBL").css("width", neg + "%").attr("aria-valuenow", neg).text(neg !== 0 ? neg + "%" : "");
+            $("#playpausePBR").css("width", pos + "%").attr("aria-valuenow", pos).text(pos !== 0 ? pos + "%" : "");
 
+            current_progress = parseInt(controller.axis[settings.forwardrewind.axis] * 100);
+            pos = current_progress > 0 ? current_progress : 0;
+            neg = current_progress < 0 ? -current_progress : 0;
+            $("#forwardrewindPBL").css("width", neg + "%").attr("aria-valuenow", neg).text(neg !== 0 ? neg + "%" : "");
+            $("#forwardrewindPBR").css("width", pos + "%").attr("aria-valuenow", pos).text(pos !== 0 ? pos + "%" : "");            
+
+            current_progress = parseInt(controller.axis[settings.volume.axis] * 100);
+            pos = current_progress > 0 ? current_progress : 0;
+            neg = current_progress < 0 ? -current_progress : 0;
+            $("#volumePBL").css("width", neg + "%").attr("aria-valuenow", neg).text(neg !== 0 ? neg + "%" : "");
+            $("#volumePBR").css("width", pos + "%").attr("aria-valuenow", pos).text(pos !== 0 ? pos + "%" : "");
+        }
+    });
     // event input change
     $("#playpause").change(function() {
         settings.playpause.axis = $(this).val();
@@ -39,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     $("#playpauseThreshold").change(function() {
         settings.playpause.threshold = parseInt($(this).val())/100;
-        chrome.storage.sync.set(settings);
+        chrome.storage.sync.set(settings);        
     });
     $("#forwardrewind").change(function() {
         settings.forwardrewind.axis = $(this).val();
